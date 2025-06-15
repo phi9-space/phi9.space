313eb14399b8c53600d0bb1ef010c6f4844302fb
Shubham Attri <attri.shubham148@gmail.com>
2025-06-15 16:15:23 +0400

    chore: comment out PHI9 logo display in home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,7 +4,7 @@ import './Home.css';
 const Home = () => {
   return (
     <div className="home-container">
-      <img src="/PHI9 LOGO.svg" alt="PHI9 Logo" className="home-logo" />
+      {/* <img src="/PHI9 LOGO.svg" alt="PHI9 Logo" className="home-logo" /> */}
       <p className="home-tagline">
         We are building GPS denied Navigation system for the next Trillion Dollar Opportunity.
       </p>
