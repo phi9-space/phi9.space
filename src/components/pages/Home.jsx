dbdb44dd6f980c8dd3d323640b9fe816ffe6b2eb
Shubham Attri <a3fckx@proton.me>
2025-06-26 02:31:49 +0530

    update"

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="home-container">
       {/* <img src="/PHI9 LOGO.svg" alt="PHI9 Logo" className="home-logo" /> */}
       <p className="home-tagline">
-        We are building GPS denied Navigation system for the next Billion Autonous Systems.
+        We are building GPS denied Navigation system for the next Billion Autonomous Systems.
       </p>
     </div>
   );
